506238f22aadf54b1cde4a62191554b2aef4d8dc
om101deore <theovdeore.2003@gmail.com>
2022-10-30 22:44:51 +0530

    Nothing

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -55,6 +55,7 @@ public class PlayerMovement : MonoBehaviour
         {
             return;
         }
+        //dbiuwbdiuabuiw
 
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
     }
